3d0a73be0af4b0969562cc30a701d747483e0bbc
Sebastian Ospino <sospino110@gmail.com>
2024-04-12 17:05:36 -0500

    log database startup-script

--- init_log_database.sh
+++ init_log_database.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sudo apt update && sudo apt install -y influxdb
-sudo sed -i 's/^  bind-address.*/  bind-address = "${INTERFACE_LOG_DATABASE}:${PORT_LOG_DATABASE}"/' /etc/influxdb/influxdb.conf
-sudo sed -i 's/^  max-connections.*/  max-connections = ${MAX_CONN_LOG_DATABASE}/' /etc/influxdb/influxdb.conf
+sudo apt install influxdb-client
+sudo cp /etc/influxdb/influxdb.conf influxdb.txt
+sed -i 's/^  # bind-address.*/  bind-address = "${INTERFACE_LOG_DATABASE}:${PORT_LOG_DATABASE}"/' influxdb.txt
+sed -i 's/^  # max-connection-limit.*/  max-connection-limit = ${MAX_CONN_LOG_DATABASE}/' influxdb.txt
+envsubst < influxdb.txt > influxdb.conf
+sudo mv influxdb.conf /etc/influxdb/influxdb.conf
 sudo systemctl unmask influxdb.service
 sudo systemctl start influxdb
 sudo systemctl enable influxdb.service
